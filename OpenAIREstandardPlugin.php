546a77ebd7409cf0887125cbe17bc090dc8e8db0
Academic-Journals.cz <radek.gomola@ruk.cuni.cz>
2026-03-20 09:08:36 +0100

    fixed the old import style

--- OpenAIREstandardPlugin.php
+++ OpenAIREstandardPlugin.php
@@ -21,6 +21,8 @@ use PKP\db\DAORegistry;
 use PKP\plugins\GenericPlugin;
 use PKP\plugins\Hook;
 use PKP\plugins\PluginRegistry;
+use APP\plugins\generic\openAIREstandard\OAIMetadataFormatPlugin_OpenAIREstandard;
+use APP\plugins\generic\openAIREstandard\OpenAIREstandardGatewayPlugin;
 
 class OpenAIREstandardPlugin extends GenericPlugin {
 
@@ -30,9 +32,7 @@ class OpenAIREstandardPlugin extends GenericPlugin {
     function register($category, $path, $mainContextId = null) {
         $success = parent::register($category, $path, $mainContextId);
         if ($success && $this->getEnabled($mainContextId)) {
-            $this->import('OAIMetadataFormatPlugin_OpenAIREstandard');
             PluginRegistry::register('oaiMetadataFormats', new OAIMetadataFormatPlugin_OpenAIREstandard($this), $this->getPluginPath());
-            $this->import('OpenAIREstandardGatewayPlugin');
             PluginRegistry::register('gateways', new OpenAIREstandardGatewayPlugin($this), $this->getPluginPath());
 
             # Handle COAR resource types in section forms
